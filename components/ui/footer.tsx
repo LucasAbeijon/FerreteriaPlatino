b09c614696d9ef8393202144cfd27db25d183b85
Lucas Abeijon <54157428+LucasAbeijon@users.noreply.github.com>
2024-08-18 20:40:31 +0000

    Update Copyright

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -134,7 +134,7 @@ export default function Footer() {
             </ul>
 
             {/* Copyrights note */}
-            <div className="text-gray-400 text-sm mr-4">&copy; Cruip.com. All rights reserved.</div>
+            <div className="text-gray-400 text-sm mr-4">&copy; Copyright Ferretería Platino de Uruguay.</div>
 
           </div>
 
